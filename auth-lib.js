865095bfea3324ee6449e7cfbb62a87e457f960f
stuffic <pietrovna.a@mail.ru>
2020-04-02 12:55:31 +0500

    first changes

--- auth-lib.js
+++ auth-lib.js
@@ -1,17 +1,59 @@
+let all_users = [];
+
 // Возвращает массив всех пользователей.
-function users() {}
+function users()
+{
+    return all_users;
+}
 
 //Создает нового пользователя с указанным логином username и паролем password, возвращает созданного пользователя.
-function createUser(name, password) {}
+function createUser(name, password)
+{
+    let obj = new Object();
+    obj.name = name;
+    obj.password = password;
+    obj.groups = ["all"];
+    all_users.push(obj);
+    return obj.name;
+}
 
 // Удаляет пользователя user
-function deleteUser(user) {}
+function deleteUser(user)
+{
+    let searchuser = user;
+    let indexOfUser = -1;
+    for (let i = 0; i < all_users.length; i++)
+    {
+        if (all_users[i].name == searchuser) {
+            indexOfUser = i;
+            break;
+        }
+    }
+
+    if (!user)
+    {
+        throw new Error("Неккоретно введенные данные");
+    }
+
+    if (indexOfUser === -1)
+    {
+        throw new Error("Нельзя удалить несуществующего пользователя");
+    }
+
+    all_users.splice(indexOfUser, 1);
+
+}
 
 // Возвращает массив групп, к которым принадлежит пользователь user
-function userGroups(user) {}
+function userGroups(user)
+{
+
+}
 
 // Добавляет пользователя user в группу group
-function addUserToGroup(user, group) {}
+function addUserToGroup(user, group)
+{
+}
 
 // Удаляет пользователя user из группы group. Должна бросить исключение, если пользователя user нет в группе group
 function removeUserFromGroup(user, group) {}
